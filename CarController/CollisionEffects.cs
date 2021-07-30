fe97e3db56930f867be5573870f814fc2adef7d8
SpawnCampGames <60554748+SpawnCampGames@users.noreply.github.com>
2021-07-30 18:19:14 -0500

    Create CollisionEffects.cs

---
+++ CarController/CollisionEffects.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class CollisionEffects : MonoBehaviour
+{
+
+    public GameObject hitEffectPrefab;
+
+    private void OnCollisionEnter(Collision other)
+    {
+        Instantiate(hitEffectPrefab, other.GetContact(0).point, Quaternion.identity);
+    }
+}
